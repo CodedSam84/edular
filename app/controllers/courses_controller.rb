f8c5d38f2a9b8f2b99ed857098fcb2b5a328a3c1
samuel <swumatob@yahoo.co.uk>
2021-05-13 16:49:32 +0100

    Added user references to course model

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,28 +1,23 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
 
-  # GET /courses or /courses.json
   def index
     @courses = Course.all
   end
 
-  # GET /courses/1 or /courses/1.json
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
-
+    @course.user = current_user
     respond_to do |format|
       if @course.save
         format.html { redirect_to @course, notice: "Course was successfully created." }
@@ -34,7 +29,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     respond_to do |format|
       if @course.update(course_params)
@@ -47,7 +41,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
     @course.destroy
     respond_to do |format|
@@ -57,12 +50,10 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(:title, :description)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,18 @@ Course.destroy_all
 
 puts "Creating records...."
 
+User.create!(
+  email:"sammy@yahoo.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
 30.times do
 	Course.create!(
-		title: Faker::Educator.course_name,
-	  description: Faker::Quote.famous_last_words
-	)
+	  title: Faker::Educator.course_name,
+	  description: Faker::Quote.famous_last_words,
+	  user_id: User.first.id 
+    )
 end
 
 puts "Records successfully created!"
